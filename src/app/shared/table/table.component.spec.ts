42abf0b46c3358449bf6c5d5074ff218763ba1c1
Faizou-Aremou <faizouaremou@gmail.com>
2022-02-23 16:17:13 +0100

    1 file modified

--- src/app/shared/table/table.component.spec.ts
+++ src/app/shared/table/table.component.spec.ts
@@ -1,17 +1,15 @@
-import { DebugElement } from "@angular/core";
-import { TableConfig } from "../models/table-config.model";
-import { TableComponent } from "./table.component";
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { By } from "@angular/platform-browser";
+import { DebugElement } from '@angular/core';
+import { TableConfig } from '../models/table-config.model';
+import { TableComponent } from './table.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 export interface qualityControlResult {
   control: string;
   minValue: number;
   maxValue: number;
 }
-describe("TableComponent", () => {
-  let component: TableComponent<qualityControlResult>;
-  let fixture: ComponentFixture<TableComponent<qualityControlResult>>;
+describe('Mat Table Custom', () => {
   let expectedElement: Array<qualityControlResult>;
   let expectedTotalInBackEnd: number;
   let expectedDisplayedColumns: Array<string>;
@@ -31,46 +29,49 @@ describe("TableComponent", () => {
   );
 
   beforeEach(() => {
-    fixture =
+    expectedElement = [{ control: 'temperature', minValue: 20, maxValue: 40 }];
+    expectedTotalInBackEnd = 10;
+    expectedDisplayedColumns = ['control', 'minValue', 'maxValue'];
+    expectedDisplayedColumnsLabels = ['Control', 'Min Value', 'Max Value'];
+  });
+
+  it('should affect tableRows with merge of displayedColumns and action ', () => {
+    const fixture =
       TestBed.createComponent<TableComponent<qualityControlResult>>(
         TableComponent
       );
-    component = fixture.componentInstance;
     fixture.detectChanges();
-    expectedElement = [{ control: "temperature", minValue: 20, maxValue: 40 }];
-    expectedTotalInBackEnd = 10;
-    expectedDisplayedColumns = ["control", "minValue", "maxValue"];
-    expectedDisplayedColumnsLabels = ["Control", "Min Value", "Max Value"];
-    //editElementEl = fixture.debugElement.nativeElement.querySelector('#actions-button')
-
-    //         sortData: DebugElement;
-    //         changePage: DebugElement;
-  });
-
-  it("should affect tableRows with merge of displayedColumns and action ", () => {
-    component.displayedColumns = ["name", "id", "description"];
+    const component = fixture.componentInstance;
+    component.displayedColumns = ['name', 'id', 'description'];
     component.ngOnInit();
     expect(component.tableRows.length).toEqual(
       component.displayedColumns.length + 1
     );
   });
 
-  it("it should have number of lines in same as elements array length ", () => {
-    const tableComponentDe: DebugElement = fixture.debugElement;
-    const tableLineDe = tableComponentDe.query(By.css('section'));
-    const trs: NodeList = tableLineDe.nativeElement;
-    console.log(trs.length)
+  it('should have number of lines in same as elements array length ', () => {
+    const fixture =
+      TestBed.createComponent<TableComponent<qualityControlResult>>(
+        TableComponent
+      );
+    fixture.detectChanges();
+    const tableElement = fixture.nativeElement.querySelector('.mat-table')!;
+    const trs = getElements(tableElement, '.cdk-row,tr');
+    console.log(trs.length);
     expect(trs.length).toEqual(expectedElement.length);
   });
 });
 
+function getElements(element: Element, query: string): Array<Element> {
+  return [].slice.call(element.querySelectorAll(query));
+}
 /**
- * critères de tests
- * 
+ * base criteria
+ *
  * le nombre de lignes doit être égale au nombre d'élément dansle tableau
- * 
+ *
  * le nombre de colones doit être égale au nombre de colonnes dans le tableau
- * 
+ *
  * les bouttons d'actions doivent bien fonctionner
- * 
+ *
  */
