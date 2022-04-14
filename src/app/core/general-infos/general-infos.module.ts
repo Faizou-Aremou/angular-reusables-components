2ca20bdfa08455af84ae7faa2346fae0c0f490f6
Faizou <faizouaremou@gmail.com>
2022-04-14 17:58:15 +0200

    delete indesirable component

--- src/app/core/general-infos/general-infos.module.ts
+++ src/app/core/general-infos/general-infos.module.ts
@@ -1,32 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GeneralInfosRoutingModule } from './general-infos-routing.module';
-import { GeneralInfosComponent } from './general-infos/general-infos.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { CompaniesComponent } from './companies/companies.component';
-import { RolesComponent } from './roles/roles.component';
-import { RolesPaneComponent } from './roles-pane/roles-pane.component';
-import { CompaniesPaneComponent } from './companies-pane/companies-pane.component';
-import { AngularMaterialModule } from 'src/material.module';
-import { InformationsComponent } from './informations/informations.component';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { GeneralInfosRoutingModule } from "./general-infos-routing.module";
+import { GeneralInfosComponent } from "./general-infos/general-infos.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import { CompaniesComponent } from "./companies/companies.component";
+import { RolesComponent } from "./roles/roles.component";
+import { AngularMaterialModule } from "src/material.module";
+import { InformationsComponent } from "./informations/informations.component";
 
 @NgModule({
   declarations: [
     GeneralInfosComponent,
     CompaniesComponent,
-     RolesComponent, 
-     RolesPaneComponent, 
-     CompaniesPaneComponent, InformationsComponent, 
+    RolesComponent,
+    InformationsComponent,
   ],
   imports: [
     CommonModule,
     GeneralInfosRoutingModule,
     SharedModule,
-    AngularMaterialModule
+    AngularMaterialModule,
   ],
-  exports:[
-    GeneralInfosComponent
-  ]
+  exports: [GeneralInfosComponent],
 })
-export class GeneralInfosModule { }
+export class GeneralInfosModule {}
