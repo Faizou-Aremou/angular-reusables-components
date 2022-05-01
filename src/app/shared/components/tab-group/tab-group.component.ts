7c440818c026d077bdf0a79ce6814eae10e3f6d8
Faizou <faizouaremou@gmail.com>
2022-05-01 16:26:22 +0200

    improve performance on ngFor excecution

--- src/app/shared/components/tab-group/tab-group.component.ts
+++ src/app/shared/components/tab-group/tab-group.component.ts
@@ -7,23 +7,25 @@ import {
   OnInit,
   QueryList,
   TemplateRef,
-} from '@angular/core';
-import { StaticTabsContentDirective } from '../../directives/tab-group/static-tabs-contents.directive';
-import { TabGroup } from '../../types/tab-group/tab-group';
+} from "@angular/core";
+import { StaticTabsContentDirective } from "../../directives/tab-group/static-tabs-contents.directive";
+import { DynamicTab } from "../../models/tab-group/dynamic-tab.model";
+import { TabGroup } from "../../types/tab-group/tab-group";
 @Component({
-  selector: 'app-tab-group',
-  templateUrl: './tab-group.component.html',
-  styleUrls: ['./tab-group.component.scss'],
-  changeDetection:ChangeDetectionStrategy.OnPush
+  selector: "app-tab-group",
+  templateUrl: "./tab-group.component.html",
+  styleUrls: ["./tab-group.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabGroupComponent<T> implements OnInit{
+export class TabGroupComponent<T> implements OnInit {
   @Input() public tabGroup: TabGroup<T> | null = null;
   @ContentChildren(StaticTabsContentDirective)
   public staticTabsContentRefs: QueryList<StaticTabsContentDirective> | null = null;
-  @ContentChild('dynamicTabsContent', { static: false })
+  @ContentChild("dynamicTabsContent", { static: false })
   public dynamicTabsContentRef: TemplateRef<any> | null = null;
   constructor() {}
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  dynamicTrackBy<T>(index: number, dynamicTab: DynamicTab<T>) {
+    return dynamicTab.label;
   }
-
 }
