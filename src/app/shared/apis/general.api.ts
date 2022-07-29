1a8f9f152613426090f9a184052ced5c85bc3505
Faizou <faizouaremou@gmail.com>
2022-07-29 15:34:18 +0200

    feat: add more functions and test

--- src/app/shared/apis/general.api.ts
+++ src/app/shared/apis/general.api.ts
@@ -6,6 +6,27 @@ export function calculateSizeFor<T extends { size: number }>(
     return size + element.size;
   }, initialSize);
 }
+/**
+ * 
+ * @param number 
+ * @returns 
+ */
 export function removeOne(number: number): number {
   return number - 1;
+}
+
+
+
+
+
+//--------------------------------------Predicates---------------------------------------------
+/**
+ * numberCompare:: T, T -> boolean
+ */
+ export function isSupNumber<T>(element1: T, element2: T): boolean {
+  return element1 >= element2;
+}
+
+export function isInfNumber<T>(element1: T, element2: T): boolean {
+  return element1 <= element2;
 }
